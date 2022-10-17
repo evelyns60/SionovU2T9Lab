85c7ed645b5f2d6a2cd8615f6620c67f36f3203b
Evelyn Sionov <evesionov@gmail.com>
2022-10-16 20:22:16 -0400

    10/16/22 home push

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String[]args) {
+        System.out.println(Math.toDegrees(Math.PI/2));
+        System.out.println(Math.toDegrees((Math.PI/2) * 3));
+        System.out.println(Math.toRadians(90));
+        System.out.println(Math.toRadians(270));
+
+
+    }
+}
